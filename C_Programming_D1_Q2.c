f5282adc1f889aacc1882d80af25e150cd1428ce
priyanshufocusss-ai <priyanshufocusss@gmail.com>
2025-11-03 23:49:48 +0530

    This is Ques 2 of Day 1 Challenge

---
+++ C_Programming_D1_Q2.c
@@ -0,0 +1,10 @@
+/*Write a C program to swap two numbers input by user using Bitwise XOR(^)*/
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    scanf("%d%d", &num1, &num2);
+    num1 = num1^num2;
+    num2 = num1^num2;
+    num1 = num1^num2;
+    printf("After swapping the numbers the resukt is: %d and %d",num1, num2);
+}
